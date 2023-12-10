e7c9457c505de94e8109c73318c51dbfed40f7da
Alexey <alexk550@mail.ru>
2023-12-10 21:16:36 +0700

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import aiogram
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    aiogram.F('xxx')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
